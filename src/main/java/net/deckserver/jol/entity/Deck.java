d9311af4c37c21088950fbe44726191fccb737ae
Shannon Dowley <shannon.dowley@gmail.com>
2025-12-23 15:47:35 +1000

    Add new 'Deck' entity for user-created decks

---
+++ src/main/java/net/deckserver/jol/entity/Deck.java
@@ -0,0 +1,29 @@
+package net.deckserver.jol.entity;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+@Entity
+public class Deck extends PanacheEntity {
+
+    @ManyToOne(optional = false)
+    public User user;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    public String contents;
+    public String name;
+    public String summary;
+
+    public static Deck create(User user, String name, String contents, String summary) {
+        Deck deck = new Deck();
+        deck.user = user;
+        deck.name = name;
+        deck.contents = contents;
+        deck.summary = summary;
+        deck.persist();
+        return deck;
+    }
+}
